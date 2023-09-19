6fe48088e18ab0374b7d486e56242383b481a004
ianodhiambo1 <ian.odhiambo@strathmore.edu>
2023-09-19 20:36:03 +0300

    not feeling it. css failing. --tommorow

--- html/le_jersey.php
+++ html/le_jersey.php
@@ -27,32 +27,37 @@ while ($row = $result->fetch_assoc()) {
     <body>
         <?php include 'inc/header.php' ?>
         <div class="wrap">
-            <div class="title"><?php echo $row['name']." Jersey "?></div>
-            <div class="tshirt_img"  ><img src="images/<?php echo $row['image_url']?>" alt="<?php echo $row['name']." Jersey "?>"></div>
+            <div class="img_title">
 
+                <div class="title"><?php echo $row['name']." Jersey "?></div>
+                <div class="tshirt_img"  ><img src="images/<?php echo $row['image_url']?>" alt="<?php echo $row['name']." Jersey "?>"></div>
+            </div>
 
-            <ul class="divBTN">
-                <li><button onclick="desc()">Description</button></li>
-                <li><button onclick="delivery()">Delivery Info</button></li>
-                <li><button onclick="review()">Reviews</button></li>
-            </ul>
+            <div class="details_img">
 
-            <div class="adjdiv" id="adjdiv">
-                <p>Description</p>
+                <ul class="divBTN">
+                    <li><button onclick="desc()">Description</button></li>
+                    <li><button onclick="delivery()">Delivery Info</button></li>
+                    <li><button onclick="review()">Reviews</button></li>
+                </ul>
+    
+                <div class="adjdiv" id="adjdiv">
+                    <p>Description</p>
+                </div>
+                <div class="adjdiv" id="adjdiv2">
+                    <p>Delivery info</p>
+                </div>
+                <div class="adjdiv" id="adjdiv3">
+                    <p>Review</p>
+                </div>
+                <a href=" ">
+                    <button class="btn mr-2 mb-2 btn-icon btn-primary" type="button" 
+                    style=" width:100%;" >
+                        <span class="btn-inner-icon"><i class="fa fa-whatsapp"></i></span>
+                        <span class="btn-inner-text">ORDER NOW</span>
+                    </button>
+            </a>
             </div>
-            <div class="adjdiv" id="adjdiv2">
-                <p>Delivery info</p>
-            </div>
-            <div class="adjdiv" id="adjdiv3">
-                <p>Review</p>
-            </div>
-            <a href=" ">
-                <button class="btn mr-2 mb-2 btn-icon btn-primary" type="button" 
-                style=" width:100%;" >
-                    <span class="btn-inner-icon"><i class="fa fa-whatsapp"></i></span>
-                    <span class="btn-inner-text">ORDER NOW</span>
-                </button>
-        </a>
 
         </div>
 
